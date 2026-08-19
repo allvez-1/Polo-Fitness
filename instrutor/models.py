f37a0712c5d8e9389df832701ab4c791a88f4c05
Mayconlpn <mayconlpn@gmail.com>
2026-08-19 00:11:46 -0300

    consetando

--- instrutor/models.py
+++ instrutor/models.py
@@ -3,32 +3,20 @@ from django.db import models
 
 class Instrutor(models.Model):
     nome = models.CharField(max_length=150)
-
     cpf = models.CharField(
         max_length=14,
         unique=True
     )
-
-    email = models.EmailField(
-        unique=True
-    )
-
-    telefone = models.CharField(
-        max_length=20
-    )
-
-    registro_profissional = models.CharField(
-        max_length=30,
-        unique=True
-    )
-
     especialidade = models.CharField(
         max_length=100
     )
-
+    cref = models.CharField(
+        max_length=30,
+        unique=True
+    )
     ativo = models.BooleanField(
         default=True
     )
-
     def __str__(self):
         return self.nome
+    
